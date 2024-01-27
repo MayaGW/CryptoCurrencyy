83c9d8c986aa886548fa57172e612855d38dee8d
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-01-27 20:15:00 +0100

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Maya Ghamloush on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
